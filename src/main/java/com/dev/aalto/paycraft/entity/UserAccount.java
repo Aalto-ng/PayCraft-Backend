1725412da537247b1668928ef1b55123a2b11f99
devhnry <devwhenry@gmail.com>
2024-09-05 13:56:21 +0100

    feat(jwtservice): implemented service to generate access and refresh tokens, updated user enitity

--- src/main/java/com/dev/aalto/paycraft/entity/UserAccount.java
+++ src/main/java/com/dev/aalto/paycraft/entity/UserAccount.java
@@ -2,11 +2,17 @@ package com.dev.aalto.paycraft.entity;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
 
 @Entity @Getter @Setter
 @AllArgsConstructor @NoArgsConstructor
 @ToString @Table(name = "user_account")
-public class UserAccount extends BaseEntity{
+public class UserAccount extends BaseEntity implements UserDetails {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -16,4 +22,34 @@ public class UserAccount extends BaseEntity{
     private String phoneNumber;
     private String jobTitle;
     private String password;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(new SimpleGrantedAuthority("ROLE_USER"));
+    }
+
+    @Override
+    public String getUsername() {
+        return emailAddress;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
